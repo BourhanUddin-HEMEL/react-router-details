bfc0d344fc6f7b17f08e6d98465961f5b67a5a82
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-04-05 16:46:26 +0600

    create components

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <a href="">Home</a>
+      <a href="">About </a>
+      <a href="">Contact</a>
+      <a href="">Login</a>
+    </div>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,32 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import About from "./Components/About/About";
+import Contact from "./Components/Contact/Contact";
 import "./index.css";
+import Home from "./Components/Home/Home";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App></App>
+//   },
+//   {
+//     path: '/about',
+//     element: <About></About>
+//   },
+//   {
+//     path:'/contact',
+//     element: <Contact></Contact>
+//   }
+// ]);
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App></App>
-  },
-  {
-    path: '/about',
-    element: <div>Contact us Now</div>
-  },
-  {
-    path:'/contact',
-    element: <div>This is contact page</div>
+    path: '/',
+    element: <Home></Home>
   }
-]);
+])
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <RouterProvider router={router}></RouterProvider>
